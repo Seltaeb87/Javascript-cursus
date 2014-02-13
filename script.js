ec95da606b409b6886ec042d7e1432e524e6fdee
Bastiaan Bitter <bastiaan@skybluemedia.nl>
2014-02-13 22:24:02 +0100

    Javascript file aangemaakt met methods in objecten

---
+++ script.js
@@ -0,0 +1,29 @@
+var settings = {
+     sandbox: 'sandbox',
+}
+
+var gps{
+    startInterval: function(){}
+    updatePosition: function(){}
+    checkLocations: function(){}
+    eventTarget: function(){}
+    init: function(){}
+    	
+}
+
+var map{
+    generateMap: function(){}
+    updatePosition: function(){}
+}
+
+///Debug object waar meerdere debugs worden afgehandeld
+var debug{
+    geoErrorHandler:function(){}
+    debugMessage: function(){}
+    setCustomDebugging: function(){}
+}
+
+var helper = {
+    isNumber: function(){
+    }
+}
